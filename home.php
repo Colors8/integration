c0efc1b0c78120337b61f48a92cc5a1f7c2dfeae
Colors8 <tchoreloffalexandra@MacBook-Pro-de-Tchoreloff.local>
2015-10-22 18:43:25 +0200

    base
    base du github

---
+++ home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Survival Kit</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="">
+</head>
+<!-- Header general -->
+<header>
+    <img src="img/logo-wordpress.png" alt="Logo">
+    <button><a href="#">Créez votre propre kit</a></button>
+</header>
+
+<body> 
+<!-- Header home -->
+<section class="home">
+	<span>SURVIVEZ À TOUT ENVIRONNEMENT</span>	
+</section>
+
+<!-- Content of the home page -->
+<div class="choices">
+	<a href="#">
+		<img class="bg" src="img/mer.png" alt="">
+		<p>MER</p>
+		<img class="icon" src="img/icone-mer.png" alt="Picto mer">
+	</a>
+	<a href="#">
+		<img class="bg" src="img/jungle.png" alt="">
+		<p>JUNGLE</p>
+		<img class="icon" src="img/icone-jungle.png" alt="Picto jungle">
+	</a>
+	<a href="#">
+		<img class="bg" src="img/montagne.png" alt="">
+		<p>MONTAGNE</p>
+		<img class="icon" src="img/icone-montagne.png" alt="Picto montagne">
+	</a>
+	<a href="#">
+		<img class="bg" src="img/ile-deserte.png" alt="">
+		<p>ILE DESERTE</p>
+		<img class="icon" src="img/icone-ile.png" alt="Picto île déserte">
+	</a>
+	<a href="#">
+		<img class="bg" src="img/" alt="">
+		<p>DESERT</p>
+		<img class="icon"src="img/icone-desert.png" alt="Picto désert">
+	</a>
+</div>
+</body>
+</html>
