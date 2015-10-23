fc9673f4d99dbe97e8b2af383d34785d4584430b
Colors8 <tchoreloffalexandra@macbook-pro-de-tchoreloff.home>
2015-10-23 21:39:41 +0200

    HTML + CSS
    Réorganisation de l’HTML et modification du CSS, pour reproduire la
    maquette

--- home.php
+++ home.php
@@ -3,48 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <title>Survival Kit</title>
+    <link rel="stylesheet" type="text/css" href="reset.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="">
+    
 </head>
 <!-- Header general -->
 <header>
     <img src="img/logo-wordpress.png" alt="Logo">
-    <button><a href="#">Créeez votre propre kit</a></button>
+    <button><a href="#">Créez votre propre kit</a></button>
+    <!-- On ajoutera peut-être un p la pour faire un petit sous-titre de blabla ;) -->
 </header>
 
 <body> 
 <!-- Header home -->
-<section class="home">
-	<span>SURVIVEZ À TOUT ENVIRONNEMENT</span>	
-</section>
+	<div class="home">
+		<span>SURVIVEZ À TOUT ENVIRONNEMENT</span>	
+	</div>
 
-<!-- Content of the home page -->
-<div class="choices">
-	<a href="#">
-		<img class="bg" src="img/mer.png" alt="">
-		<p>MER</p>
-		<img class="icon" src="img/icone-mer.png" alt="Picto mer">
-	</a>
-	<a href="#">
-		<img class="bg" src="img/jungle.png" alt="">
-		<p>JUNGLE</p>
-		<img class="icon" src="img/icone-jungle.png" alt="Picto jungle">
-	</a>
-	<a href="#">
-		<img class="bg" src="img/montagne.png" alt="">
-		<p>MONTAGNE</p>
-		<img class="icon" src="img/icone-montagne.png" alt="Picto montagne">
-	</a>
-	<a href="#">
-		<img class="bg" src="img/ile-deserte.png" alt="">
-		<p>ILE DESERTE</p>
-		<img class="icon" src="img/icone-ile.png" alt="Picto île déserte">
-	</a>
-	<a href="#">
-		<img class="bg" src="img/" alt="">
-		<p>DESERT</p>
-		<img class="icon"src="img/icone-desert.png" alt="Picto désert">
-	</a>
-</div>
-</body>
+	<!-- Content of the home page -->
+	<ul class="choices">
+		<li class="enviro">	
+			<a href="#">
+				<div class="content" style="background-image: url(img/mer.png);">
+					<p>MER</p>
+					<img class="icon" src="img/icone-mer.png" alt="Picto mer">
+				</div>
+			</a>
+		</li><!--
+		--><li class="enviro">	
+			<a href="#">
+				<div class="content" style="background-image: url(img/jungle.png)">
+					<p>JUNGLE</p>
+					<img class="icon" src="img/icone-jungle.png" alt="Picto jungle">
+				</div>
+			</a>
+		</li><!--
+		--><li class="enviro">	
+			<a href="#">
+				<div class="content" style="background-image: url(img/montagne.png)">
+					<p>MONTAGNE</p>
+					<img class="icon" src="img/icone-montagne.png" alt="Picto montagne">
+				</div>
+			</a>
+		</li><!--
+		--><li class="enviro">	
+			<a href="#">
+				<div class="content" style="background-image: url(img/ile-deserte.png)">
+					<p>ILE DESERTE</p>
+					<img class="icon" src="img/icone-mer.png" alt="Picto mer">
+				</div>
+			</a>
+		</li><!--
+		--><li class="enviro">	
+			<a href="#">
+				<div class="content" style="background-image: url(img/desert.png)">
+					<p>DESERT</p>
+					<img class="icon" src="img/icone-ile.png" alt="Picto ile deserte">
+				</div>
+			</a>
+		</li>
+	</ul>
+	</body>
 </html>
