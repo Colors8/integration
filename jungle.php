f56585f18119abcef1d5f1889838292e3ef9dd54
lionelquellery <lionel.quellery@hetic.net>
2015-10-28 12:36:04 +0100

    update template

--- jungle.php
+++ jungle.php
@@ -1,87 +1,107 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" type="text/css" href="reset.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+    
+    
+    
+    
+        <link rel="stylesheet" href="css/style.css">
+    
+    
+    
+  </head>
 
-<!-- Header de la page-->
+    
 
 <body>
-	<ul class="menu">
-		<a href="#"><li><p>Accueil</p><img src="img/round.png" alt="Accueil"></li></a>
-		<a href="#situations"><li><p>Situations</p><img src="img/current-round.png" alt="Situations"></li></a>
-		<a href="#"><li><img src="img/round.png" alt="Situation 1"></li></a>
-		<a href="#"><li><img src="img/round.png" alt="Situation 2"></li></a>
-		<a href="#"><li><img src="img/round.png" alt="Situation 3"></li></a>
-		<a href="#"><li><p>Ingrédients</p><img src="img/round.png" alt="Ingrédients"></li></a>
-	</ul>
-			<section class="presentation">
-				<div class="left-bloc" style="background-image: url(img/jungle-pres.png)">
-					<img src="img/logo-wordpress-blanc.png" alt="Logo">
-					<h2>Survie dans la </h2>
-					<h3>JUNGLE</h3>
-				</div>
-				<div class="info-bloc">
-					<h4>HUMIDE ET SAUVAGE</h4>
-					<p><span>La jungle également appelée forêt tropicale, est caractérisé par un climat, une faune et une flore très spécifique.</span><span>En effet, le climat y est chaud et humide toute l’année, c’est la raison pour laquelle toutes les jungles se situent au niveau de l’équateur. Les précipitations sont assez variables tant dans le temps que dans l’espace . La température moyenne y est de 26 degrés.
-		La flore est également facilement identifiable : de grands arbres centenaire, de nombreuses arbustes, fleurs et autre fougères capable de supporter ce climat particulier.  Enfin la faune y est très dense de nombreux animaux sauvages et insectes ont élu domicile dans ces régions.</span><span>Sous ses aires de forêt accueillante, la jungle est pourtant un des territoires les plus hostiles pour l’Homme. Prédateurs sauvages, plantes, animaux et insectes venimeux, orage tropicaux violents, autant d’éléments qui rendent la survie en pleine jungle très périlleuse pour une personne non-experimentée.</span> </p>
-					<div>
-						<h5>PRINCIPAUX DANGERS</h5>
-						<div class="intro-situations">
-							<div class="intro one">
-								<a href="#situation-one">
-									<h6>Egaré</h6>
-									<p>Dans une fôret dense en végétation où il y a peu de repères, il devient facile de ne pas retrouver son chemin.</p>
-								</a>
-							</div>
-							<div class="intro two">
-								<a href="#">
-									<h6>Orage</h6>
-									<p>Situés dans des zones tropicales, la jungle est propoice aux orages. L’humidité et la chaleur les rendent violent.</p>
-								</a>
-							</div>
-							<div class="intro three">
-								<a href="#">
-									<h6>Animal sauvage</h6>
-									<p>La jungle est le territoire de nombreux animaux dangereux, discret, qui peuvent vous tomber dessus au dernier moment.</p>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			<section class="situation-one" style="background-image: url(img/jungle-bg1.png)">
-				<div class="green" style="background-color: #3f754b"></div>
-				<div class="left-info">
-					<img src="img/logo-wordpress.png" alt="Logo">
-					<span>A PROPOS</span>
-					<p><span>Parti pour une petite randonnée entre amis, vous vous enfoncez petit à petit dans la jungle. Sûr de vos capacités vous avez fait l’erreur de partir sans guide en pensant que vous alliez pouvoir facilement rebrousser chemin pour rentrer. Or la jungle est un milieu à la végétation  très dense et dans lequel très peu de sentier existe … La nuit commence à tomber et vous voila bel et bien perdu !<span>… La prochaine fois vous penserez à partir accompagné d’un guide local !</span></p>
-				</div>
-				<div class="needed">
-					<p>Egare</p>
-					<div class="content-slider">
-					<p>Vous aurez besoin de <strong>5 ingrédients</strong></p>
-						<div class="slider">
-							<div class="ingredient-one">
-								<div class="first-ingredient">
-										<img src="" alt="Machette">
-										<div class="separator"></div>
-										<p>Dégager son chemin</p>
-								</div>
-							<p>Machette</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			<section class="situation-two">
-			</section>
-			<section class="situation-three">
-			</section> 
 
-	
-</body>
+<!--   <nav class="menu">
+    <ul>
+      <li><a href="#panel-name-1">PANEL 1</a></li>
+      <li><a href="#panel-name-2">PANEL 2</a></li>
+      <li><a href="#panel-name-3">PANEL 3</a></li>
+      <li><a href="#panel-name-4">PANEL 4</a></li>
+    </ul>
+  </nav> -->
+
+	<section class="panel" id="panel-name-1">
+ <div class="sidebar">
+  <h1 class="sidebar-template-title">Survie dans la </br> 
+  <strong>jungle</strong></h1>
+</div>
+
+
+
+
+<div class="contain-right">
+
+<div class="template-top">
+  <h1 class="title-template-right-top"> HELLO </h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="template-situation">
+    <h1 class="title-template-right-top-1">Principaux Dangers</h1>
+    
+  
+  </div>
+</div>
+   
+  </section>
+  <section class="panel" id="panel-name-2">
+  <div class="title-template">EGARE</div>
+   <div class="float-box">
+    <h1 class="title-template-1">A PROPOS</h1> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     
+   </div>
+
+    
+  </section>
+  <section class="panel" id="panel-name-3">
+   <div class="title-template">ORAGE</div>
+   <div class="float-box">
+    <h1 class="title-template-1">A PROPOS</h1> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     
+   </div>
+    
+  </section>
+  <section class="panel" id="panel-name-4">
+   <div class="title-template">Aniaml</div>
+   <div class="float-box">
+    <h1 class="title-template-1">A PROPOS</h1> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     
+   </div>
+    
+  </section>
+  
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="js/index.js"></script>
+
+    
+    
+    
+  </body>
 </html>
